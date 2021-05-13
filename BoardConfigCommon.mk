133a5e3b3973e00b5e153a0320932a42bfeb73b5
Mesquita <mesquita@aospa.co>
2021-05-12 21:43:03 -0300

    sm8250-common: Do not explicitly include qcom sepolicy
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,7 +158,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2021-03-01
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
