66dacce586c100743b0483eeaa7f8167e7060a77
Jake Weinstein <jake@aospa.co>
2021-05-12 23:13:19 -0300

    sm8250-common: Define TARGET_BOARD_PLATFORM in device  makefile
    This allows us to use it in QC Split Product Makefiles
    
    Change-Id: Ibd7493eb8c266b524a8f51aad658837798cc3d2d
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,7 +129,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_BOARD_PLATFORM := kona
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
