56b554f2556e41744423ef1f9c0ee63e1a5a826c
Mesquita <mesquita@aospa.co>
2021-05-12 21:38:12 -0300

    sm8250-common: Drop livedisplay
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- kona.mk
+++ kona.mk
@@ -237,10 +237,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.kona
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
