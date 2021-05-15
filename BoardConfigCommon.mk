7ba587f50165b6392e3e6d4d48cbb4375f776f29
Alexander Koskovich <zvnexus@outlook.com>
2021-05-15 01:03:01 -0300

    sm8250-common: Move compatibility matrix to QCOM common.
    Change-Id: Iafb6a3f7eb4130fdc5264abb04142fe5580a1fb6
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,8 +76,8 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/xiaomi_vendor_framework_compatibility_matrix.xml
 
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
