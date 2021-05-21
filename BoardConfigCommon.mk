633bb48150973eeae246861a06a67273b7e85dc5
Mesquita <mesquita@aospa.co>
2021-05-21 11:35:40 -0300

    sm8250-common: Drop fwk detect props
     * Already defined in pa build
    
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,7 +105,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
-TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
